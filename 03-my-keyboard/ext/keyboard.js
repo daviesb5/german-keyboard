8e344fa0fe99e5f40d2c6bcecad995dbd7fe4359
daviesb5 <braedondavies@gmail.com>
2020-03-18 16:27:27 -0600

    Updating files

--- 03-my-keyboard/ext/keyboard.js
+++ 03-my-keyboard/ext/keyboard.js
@@ -11,19 +11,14 @@ var requiredComboLength = 0;
 //Special Characters [":", "s", "^", "'", "`", "-", "="];
 var specialCharArray = [":", "s", "^", "'", "`", "-", "="];
 var asciiArray = [186, 83, 54, 222, 192, 189, 187];
+//textareaInput and comboBox
+textareaInput = document.getElementById("typingTextbox").value;
+document.getElementById("comboBox").value = textareaInput;
 
 function checkKeyPress() {
-    textareaInput = document.getElementById("typingTextbox").value;
-    document.getElementById("comboBox").value = textareaInput;
-
     // Determines if current key is a special character
     var lastChar = textareaInput.charAt(textareaInput.length - 1);
     keyBoolean = specialCharArray.includes(lastChar, textareaInput);
-
-    //checks that required length fits in text length
-    /*
-    requiredComboLength = findComboLength(lastChar);
-    */
 
     //alert("requiredComboLength: " + requiredComboLength);
     if (keyBoolean == true) {
@@ -34,27 +29,71 @@ function checkKeyPress() {
     }
 }
 
-/*
-function findComboLength(currentChar) {
-    // alert("currentChar: " + currentChar);
-    // determines required length
-    if (currentChar == "=") {
-        return 2;
-    } else {
-        return 3;
-    }
-}
-*/
-
 function charConversion(currentInput, finalChar) {
     alert(currentInput);
+    var specialChar;
+    // either 2 or 3 characters
+    var sliceLength;
     if (finalChar == "=" && currentInput.length >= 2) {
-        alert("Convert Character");
+        //alert("Convert Character");
+        sliceLength = 2;
     } else if (finalChar !== "=" && currentInput.length >= 3) {
-        alert("Convert Character");
+        //alert("Convert Character");
+        sliceLength = 3;
     } else {
         alert("Too short");
     }
+    specialChar = convertToSpecialChar(currentInput, finalChar, sliceLength);
+    document.getElementById("comboBox").value = specialChar;
+}
+
+function convertToSpecialChar(inputText, lastLetter, textSlice) {
+    // slices the last 2-3 characters from textbox
+    var comboSet = inputText.slice(-textSlice);
+    alert("comboSet: " + comboSet);
+
+    // special character array for conversion
+    var specialCharArray = [
+        ["A+^", "Â"],
+        ["a+^", "â"],
+        ["A+:", "Ä"],
+        ["a+:", "ä"],
+        ["E+'", "É"],
+        ["e+'", "é"],
+        ["E+`", "È"],
+        ["e+`", "è"],
+        ["E+^", "Ê"],
+        ["e+^", "ê"],
+        ["E+:", "Ë"],
+        ["e+:", "ë"],
+        ["m+-", "m̄"],
+        ["n+-", "n̄"],
+        ["O+^", "Ô"],
+        ["o+^", "ô"],
+        ["O+:", "Ö"],
+        ["o+:", "ö"],
+        ["s=", "ſ"],
+        ["S+s", "ẞ"],
+        ["s+s", "ß"],
+        ["U+:", "Ü"],
+        ["u+:", "ü"],
+        ["Y+:", "Ÿ"],
+        ["y+:", "ÿ"]
+    ];
+    // keyBoolean = specialCharArray.includes(lastChar, textareaInput);
+    var firstRow;
+    var newSpecialChar;
+    // specialCharArray[1][0].includes(lastLetter);
+    for (i = 0; i < specialCharArray.length; i++){
+        firstRow = specialCharArray[i][0].includes(lastLetter);
+        newSpecialChar = specialCharArray[i][1];
+        alert("firstRow: " + firstRow);
+        if (firstRow == true){
+            alert("newSpecialChar: " + newSpecialChar);
+            break;
+        }
+    }
+    return newSpecialChar;
 }
 
 /*
